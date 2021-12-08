056534003c42bffa04c61b6a989088855ba25bac
linminghao <172755874@qq.com>
2021-12-08 20:37:05 +0800

    完成patch函数不是同一个节点情况

--- src/mysnabbdom/createElement.js
+++ src/mysnabbdom/createElement.js
@@ -1,10 +1,25 @@
 /**
- * 真正创建节点，将vnode创建为DOM，插入倒pivot这个元素之前
+ * 真正创建节点，将vnode创建为DOM
  * @param {*} vnode
  * @param {*} pivot
  */
-export default function (vnode, pivot) {
-  console.log('目的是把虚拟节点', vnode, '插入到标杆', pivot, '之前')
-  // 创建一个DOM节点，这个节点现在还是孤儿节点
+export default function createElement(vnode) {
+  // 创建一个DOM节点
   let domNode = document.createElement(vnode.sel)
+  if (
+    vnode.text !== '' &&
+    (vnode.children === undefined || vnode.children.length === 0)
+  ) {
+    // 内部是文字的情况
+    domNode.innerText = vnode.text
+  } else if (Array.isArray(vnode.children) && vnode.children.length > 0) {
+    for (let i = 0; i < vnode.children.length; i++) {
+      // 将递归得到的DOM上树
+      domNode.appendChild(createElement(vnode.children[i]))
+    }
+  }
+  // 补充elm属性
+  vnode.elm = domNode
+  // 返回DOM
+  return vnode.elm
 }
--- src/mysnabbdom/patch.js
+++ src/mysnabbdom/patch.js
@@ -1,7 +1,10 @@
 import vnode from './vnode'
 import sameVnode from './sameVnode'
+import createElement from './createElement'
 
 export default function (oldVnode, newVnode) {
+  console.log('patch ', 'old', oldVnode, 'new', newVnode)
+
   // 判断传入的第一个参数 是DOM还是虚拟节点
   if (oldVnode.sel === '' || oldVnode.sel === undefined) {
     // DOM节点 此时要包装为虚拟节点
@@ -16,7 +19,17 @@ export default function (oldVnode, newVnode) {
   // 判断oldVnode和newVnode是不是同一个节点
   if (sameVnode(oldVnode, newVnode)) {
     // 精细化比较
+    console.log('同一个节点')
   } else {
+    console.log('不是同一个节点 插入新的删除旧的')
     // 不是同一个节点 插入新的删除旧的
+    let newVnodeElm = createElement(newVnode) //生成DOM
+    // newVnode.elm = newVnodeElm
+    // 插入新节点到老节点之前
+    if (oldVnode.elm.parentNode && newVnodeElm) {
+      oldVnode.elm.parentNode.insertBefore(newVnodeElm, oldVnode.elm)
+    }
+    // 删除老节点
+    oldVnode.elm.parentNode.removeChild(oldVnode.elm)
   }
 }
